c2d0605aa0f24f1d25c0ccbd22492cb91d57d848
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2019-11-01 21:31:00 +0100

    make example a bit more interesting (#5)

--- example/example.js
+++ example/example.js
@@ -2,11 +2,31 @@ import transformRatesToJSON from './transformRatesToJSON.js'
 
 async function exampleUsage() {
   const rates = await transformRatesToJSON()
-  const multiply = 50
-  document.querySelector('p').innerHTML = `Today 1 EUR is: <strong>${rates.USD.toFixed(
-    2
-  )} </strong>USD and <strong>${rates.GBP.toFixed(2)} </strong>GBP.<br>Today ${multiply} EUR is: <strong>${(
-    rates.USD * multiply
-  ).toFixed(2)} </strong>USD and <strong>${(rates.GBP * multiply).toFixed(2)} </strong>GBP.`
+  const submit = document.querySelector('#submitExample')
+
+  document.querySelector('#example').innerHTML = `
+  today 1 EUR is: <strong>${rates.USD.toFixed(2)} 
+  </strong>USD and <strong>${rates.GBP.toFixed(2)} </strong>GBP`
+
+  submit.disabled = false
+  // conversion
+  const convertRates = () => {
+    let inputValue = document.querySelector('#inputField').value
+    let inputCurrency = document.querySelector('#inputCurrency').value
+    let outputCurrency = document.querySelector('#outputCurrency').value
+    let inputInEUR
+    if (!isNaN(inputValue)) {
+      inputCurrency !== 'EUR'
+        ? (inputInEUR = parseFloat(inputValue) / rates[inputCurrency])
+        : (inputInEUR = parseFloat(inputValue))
+      outputCurrency !== 'EUR'
+        ? (document.querySelector('#outputField').value = (inputInEUR * rates[outputCurrency]).toFixed(2))
+        : (document.querySelector('#outputField').value = inputInEUR.toFixed(2))
+    } else {
+      document.querySelector('#outputField').value = 'please give a valid price!'
+    }
+  }
+
+  submit.addEventListener('click', convertRates)
 }
 exampleUsage()
