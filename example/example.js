6ebc1fdee19315d34d6b637b3397aebcd6de35b7
David Barton <barton.david.sv@gmail.com>
2019-11-06 21:17:38 +0100

    use strict mode

--- example/example.js
+++ example/example.js
@@ -1,3 +1,5 @@
+'use strict'
+
 import transformRatesToJSON from './transformRatesToJSON.js'
 
 async function exampleUsage() {
--- src/transformRatesToJSON.js
+++ src/transformRatesToJSON.js
@@ -22,6 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+'use strict'
+
 async function getCurrencyRatesXML() {
   try {
     let response = await fetch(
